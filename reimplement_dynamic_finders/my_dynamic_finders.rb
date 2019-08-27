6923f30b89808f34a413f11b78f4dede1642e3e3
akinov <ayumoto10@gmail.com>
2019-08-27 21:40:27 +0900

    できた

--- reimplement_dynamic_finders/my_dynamic_finders.rb
+++ reimplement_dynamic_finders/my_dynamic_finders.rb
@@ -1,2 +1,28 @@
 module MyDynamicFinders
+  private
+
+  def respond_to_missing?(symbol, include_private)
+    symbol.to_s.start_with?('find_by_')
+  end
+
+  def method_missing(name, *args)
+    name = name.to_s
+    if name.start_with?('find_by_')
+      attr_names = name.gsub(/\Afind_by_/, '').gsub(/!\z/, '').split('_and_')
+
+      conditions = attr_names.map.each_with_index { |attr, index| [attr, args[index]] }.to_h
+
+      select_data = $DATABASE.find do |data|
+        attr_names.all? do |attr|
+          data.public_send(attr) == conditions[attr]
+        end
+      end
+
+      raise if name.end_with?('!') && select_data.nil?
+
+      select_data
+    else
+      super
+    end
+  end
 end
--- reimplement_string_inquirer/my_string_inquirer.rb
+++ reimplement_string_inquirer/my_string_inquirer.rb
@@ -1,2 +1,17 @@
 class MyStringInquirer
+  def initialize(str)
+    @str = str
+  end
+
+  def respond_to_missing?(symbol, include_private)
+    symbol.to_s.end_with?('?')
+  end
+
+  def method_missing(name, *args)
+    if name.to_s.end_with?('?')
+      @env.to_s == name.to_s.gsub(/\?\z/, '')
+    else
+      super
+    end
+  end
 end
